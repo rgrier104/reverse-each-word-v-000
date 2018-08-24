f55e3e08871dd133ff48e0ff3ed31fcd163debe4
Rachel Grier <rgrier104@gmail.com>
2018-08-24 14:59:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,11 +9,3 @@ def reverse_each_word(string)
   end
   return reverse_array.join(" ")
 end
-
-def reverse_each_word_collect(string)
-  array = string.split(' ')
-  array.collect do |word|
-    word.reverse
-  end
-  return array.join(" ")
-end
