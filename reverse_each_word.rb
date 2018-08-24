868cf7fbccd91692e3f3d6255d1f79380b9845e4
Rachel Grier <rgrier104@gmail.com>
2018-08-24 14:49:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(string)
   array = string.split(' ')
+  new string = ""
   array.each do |word|
-    word.reverse
+    reverse_word = word.reverse
+    new_string = new_string << word.reverse
   end
   array
 end
