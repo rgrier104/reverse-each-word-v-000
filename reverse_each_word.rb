1b17d345770a52f278413e8220ad3e5856731e39
Rachel Grier <rgrier104@gmail.com>
2018-08-24 14:49:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   array = string.split(' ')
-  new string = ""
+  new_string = ""
   array.each do |word|
     reverse_word = word.reverse
     new_string = new_string << word.reverse
