da9f969e4301146a73dc8f394bf228e67fdd9cff
Rachel Grier <rgrier104@gmail.com>
2018-08-24 14:59:22 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(string)
+  array = string.split(' ')
+  reverse_array = []
+  array.each do |word|
+    reverse_array << word.reverse
+  end
+  array.collect do |word|
+    word.reverse
+  end
+  return reverse_array.join(" ")
+end
